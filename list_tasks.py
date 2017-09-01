8e002137e45c2d49b321a48cac2d04ea6eca7cf3
Peter Gagliardi <ptrgags@live.com>
2017-09-01 07:42:06 -0400

    List categories alphabetically

--- list_tasks.py
+++ list_tasks.py
@@ -17,13 +17,17 @@ class ListTasks:
         by_category = self.bucket_categories()
 
         # Print default category tasks first
-        for task in by_category.get(None, []):
-            print(self.format_task(task))
+        default_tasks = by_category.get(None, [])
+        if default_tasks:
+            for task in default_tasks:
+                print(self.format_task(task))
+            print()
 
         # Print the rest of the tasks
-        for cat in by_category:
+        for cat in sorted(by_category):
             if cat is None:
                 continue
-            print("\n" + cat)
+            print(cat)
             for task in by_category[cat]:
                 print(self.format_task(task))
+            print()
